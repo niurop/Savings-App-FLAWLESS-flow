7046f6f44cd26423ff6e797f4d5115f6fa962438
Marek Zielina <github@niurop.com>
2022-11-23 14:33:51 +0100

    npm install cookie-parser --save

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+var cookieParser = require("cookie-parser");
 var express = require("express");
+var fs = require("fs");
+
 var app = express();
+app.use(cookieParser());
+
+app.get("/", function (req, res) {
+  let website = fs.readFileSync("webpage/index.html", "utf8");
+  res.send(website);
+});
 
 var server = app.listen(5000, function () {
   console.log("Node server is running..");
-});
-
-app.get("/", function (req, res) {
-  res.send("HELLO WORLD!");
 });
